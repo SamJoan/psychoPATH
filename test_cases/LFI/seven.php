a72104f9d264032c42b686531f801c86b46673e4
Sam Joan Roque-Worcel <pedro@worcel.com>
2021-11-26 13:09:04 +1300

    Fix == comparison.

--- test_cases/LFI/seven.php
+++ test_cases/LFI/seven.php
@@ -2,7 +2,7 @@
 $fname=$_GET['file'];
 
 ## this one's recurrent and expected to fail when dealing with mix of no-encoding+double encoding (with only nix slashes in use)
-while(stripos($fname,"../"!=FALSE))
+while(stripos($fname,"../")!==FALSE)
 {
 	$fname=str_replace('../','',$fname);
 }
